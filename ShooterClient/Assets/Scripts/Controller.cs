ce13778d7049142b843ba59e6d86064b753ed7b3
AlexanderFredy <117285141+AlexanderFredy@users.noreply.github.com>
2024-02-28 09:58:51 +0300

    Add client and server

---
+++ ShooterClient/Assets/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] Player _player;
+    
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        _player.SetInput(h, v);
+    }
+}
